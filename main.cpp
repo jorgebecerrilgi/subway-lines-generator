97e0f022bbe70d5f144eb7c36fc8eb4a2ebd9975
Jorge Becerril <jorgebecerril@Jorges-MacBook-Air-4.local>
2024-12-28 18:34:39 -0600

    Add function to draw grid.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <SDL2/SDL.h>
 #include <iostream>
 
+void drawGrid(SDL_Renderer *renderer, int x1, int y1, int w, int h, int step) {
+    const int x2 = x1 + w, y2 = y1 + h;
+    for (int x = x1; x <= x2; x += step) {
+        SDL_RenderDrawLine(renderer, x, y1, x, y2);
+    }
+    for (int y = y1; y <= y2; y += step) {
+        SDL_RenderDrawLine(renderer, x1, y, x2, y);
+    }
+}
+
 int main(int argc, char *argv[]) {
     if (SDL_Init(SDL_INIT_VIDEO != 0)) {
         printf("SDL_Init Error: %s\n", SDL_GetError());
@@ -22,6 +32,26 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
+    SDL_Renderer *renderer = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED);
+    if (renderer == NULL) {
+        printf("SDL_CreateRenderer Error: %s\n", SDL_GetError());
+        SDL_DestroyWindow(win);
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    SDL_RenderClear(renderer);
+
+    const int GRID_WIDTH = 368;
+    const int GRID_HEIGHT = 208;
+    const int GRID_X1 = WIN_WIDTH / 2 - GRID_WIDTH / 2;
+    const int GRID_Y1 = WIN_HEIGHT / 2 - GRID_HEIGHT / 2;
+    SDL_SetRenderDrawColor(renderer, 210, 210, 210, 255);
+    drawGrid(renderer, GRID_X1, GRID_Y1, GRID_WIDTH, GRID_HEIGHT, 16); // Draw entire grid.
+
+    SDL_RenderPresent(renderer);
+
     SDL_Event e;
     bool quit = false;
     while (!quit) {
@@ -32,6 +62,7 @@ int main(int argc, char *argv[]) {
         }
     }
 
+    SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(win);
     SDL_Quit();
 
